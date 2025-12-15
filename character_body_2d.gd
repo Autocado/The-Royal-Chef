3fbc15161a971172a90314d97f755d6ee94cb621
Autocado <86090222+Autocado@users.noreply.github.com>
2025-12-15 13:41:17 +0700

    walk

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
-
+@onready var animated_sprite_2d = $CharacterBody2D/idle
 @export var speed = 80
+
 func get_input():
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -8,7 +9,20 @@ func get_input():
 	velocity = input_direction * speed
 	
 func _physics_process(_delta):
+	var axisX = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	var axisY = Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down")
+	if axisX > 0:
+		$idle.play("testwalk_d")
+		$idle.flip_h = false
+	elif axisX < 0:
+		$idle.play("testwalk_d")
+		$idle.flip_h = true
+	elif axisY > 0:
+		$idle.play("testwalk_w")
+	elif axisY < 0:
+		$idle.play("testwalk")
+	else:
+		$idle.play("white")
 	get_input()
 	move_and_slide()
-	
 	
--- icon.gd
+++ icon.gd
@@ -1,5 +1,5 @@
 extends Node2D
-@onready var animated_sprite_2d = $CharacterBody2D/AnimatedSprite2D
+@onready var animated_sprite_2d = $CharacterBody2D/idle
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
