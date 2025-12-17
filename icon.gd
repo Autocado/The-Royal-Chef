cb26e0ccf148ee182618568a972bab9031d130ac
Autocado <86090222+Autocado@users.noreply.github.com>
2025-12-17 08:16:18 +0700

    npc

--- icon.gd
+++ icon.gd
@@ -1,10 +1,10 @@
 extends Node2D
 @onready var animated_sprite_2d = $CharacterBody2D/idle
-
+@onready var animated_npc = $CharacterBody2D2/Nidle
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	animated_sprite_2d.play("white")
-
+	animated_npc.play("npc")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
