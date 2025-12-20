e3663231e3a1fcd628d755326fa42aa54d0ae39a
68219010045-rgb <68219010045@tatc.ac.th>
2025-12-20 20:37:54 +0700

    dialogue(ยังไม่เสร็จ)

---
+++ dialogue/dialogue.gd
@@ -0,0 +1,32 @@
+extends Control
+
+@export_file("*.jason") var d_file
+
+var dialogue = []
+var current_dialogue_id = 0
+
+func _ready():
+	start()
+	
+func start():
+	dialogue = load_dialogue()
+	current_dialogue_id = -1
+	next_script()
+	
+func load_dialogue():
+	var file = FileAccess.open("res://dialogue/dialogue1.json",FileAccess.READ)
+	var content = JSON.parse_string(file.get_as_text())
+	return content
+
+func _intput(event):
+	if event.is_action_pressed("ui_accept"):
+		next_script()
+	
+func next_script():
+	current_dialogue_id += 1
+	if current_dialogue_id >= len(dialogue) :
+		return
+	
+	$NinePatchRect/name.text = dialogue[current_dialogue_id]['name']
+	$NinePatchRect/text.text = dialogue[current_dialogue_id]['text']
+	
