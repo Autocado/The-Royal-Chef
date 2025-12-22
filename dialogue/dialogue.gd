a40cb8052d3bb40f553a4b366c2e145940394fc1
Autocado <86090222+Autocado@users.noreply.github.com>
2025-12-22 13:04:05 +0700

    Add dialogue overlay and highlight characters

--- dialogue/dialogue.gd
+++ dialogue/dialogue.gd
@@ -1,5 +1,6 @@
-extends Control
+extends CanvasLayer
 
+signal dialogue_started
 signal dialogue_finished
 
 @export_file("*.json") var d_file
@@ -9,12 +10,15 @@ var current_dialogue_id = 0
 var d_active=false
 
 func _ready():
-	$NinePatchRect.visible = false
+	$Overlay.visible = false
+	$UI/NinePatchRect.visible = false
 	
 func start():
 	if d_active:
 		return
-	$NinePatchRect.visible = true
+	$Overlay.visible = true
+	$UI/NinePatchRect.visible = true
+	emit_signal("dialogue_started")
 	d_active = true
 	dialogue = load_dialogue()
 	current_dialogue_id = -1
@@ -35,10 +39,11 @@ func next_script():
 	current_dialogue_id += 1
 	if current_dialogue_id >= len(dialogue) :
 		d_active = false
-		$NinePatchRect.visible = false
+		$Overlay.visible = false
+		$UI/NinePatchRect.visible = false
 		emit_signal("dialogue_finished")
 		return
 	
-	$NinePatchRect/name.text = dialogue[current_dialogue_id]['name']
-	$NinePatchRect/text.text = dialogue[current_dialogue_id]['text']
+	$UI/NinePatchRect/name.text = dialogue[current_dialogue_id]['name']
+	$UI/NinePatchRect/text.text = dialogue[current_dialogue_id]['text']
 	
--- icon.gd
+++ icon.gd
@@ -1,18 +1,28 @@
 extends Node2D
 @onready var animated_sprite_2d = $CharacterBody2D/idle
 @onready var animated_npc = $CharacterBody2D2/Nidle
-@onready var dialogue = $dialogue
+@onready var dialogue = $CharacterBody2D2/dialogue
 @onready var player = $CharacterBody2D
+var player_z_index := 0
+var npc_z_index := 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	animated_sprite_2d.play("white")
 	animated_npc.play("npc")
+	player_z_index = animated_sprite_2d.z_index
+	npc_z_index = animated_npc.z_index
+	dialogue.dialogue_started.connect(_on_dialogue_dialogue_started)
+	dialogue.dialogue_finished.connect(_on_dialogue_dialogue_finished)
 	
 
 
 func _on_dialogue_dialogue_finished() -> void:
-	pass # Replace with function body.
+	animated_sprite_2d.z_index = player_z_index
+	animated_npc.z_index = npc_z_index
 
+func _on_dialogue_dialogue_started() -> void:
+	animated_sprite_2d.z_index = 100
+	animated_npc.z_index = 100
 
 func _on_npc_interaction_area_body_entered(body: Node) -> void:
 	if Input.is_action_just_pressed("chat"):
