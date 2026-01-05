19d113eaef07bdce700e4ffa7eb56dd37dcc4eba
Autocado <86090222+Autocado@users.noreply.github.com>
2026-01-05 09:56:34 +0700

    idk just some stuff

--- area_2d_3.gd
+++ area_2d_3.gd
@@ -12,4 +12,4 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("Interact"):
 		var actionable = $".".get_overlapping_areas()
 		if actionable.size() > 0:
-			get_tree().change_scene_to_file("res://forest.tscn")
+			get_tree().change_scene_to_file("res://test.tscn")
