39079f5a1853cb812499b41485d03b21ce320051
Autocado <86090222+Autocado@users.noreply.github.com>
2026-01-19 13:43:03 +0700

    not working

--- chefturnbase.gd
+++ chefturnbase.gd
@@ -1,4 +1,63 @@
 extends CharacterBody2D
 
-func _ready() -> void:
-	$Chef.play("Stand")
+# Nodes
+@export var Health:Node
+@export var attack:Node
+@onready var entity_sprite = $Chef
+@onready var health_bar = $HealthBar
+
+# Target
+var CurrentTargets:Array
+
+
+# Stats
+var Str:int
+var Dex:int
+var Int:int
+
+
+
+func _ready():
+	entity_sprite.play("Stand")
+	attack.AttackSig.connect(AttackAnim)
+
+func _process(delta):
+	health_bar.value = Health.CurrentHP/Health.MaxHP
+
+func LoadEntity(TargetRes):
+	if TargetRes != PlayerStats:
+		entity_sprite.flip_h = true
+	Str = TargetRes.Str
+	Dex = TargetRes.Dex
+	Int = TargetRes.Int
+	Health.MaxHP = TargetRes.MaxHP
+	Health.CurrentHP = TargetRes.MaxHP
+	
+	attack.LoadSkills(TargetRes.Skills)
+	Health.Death.connect(Death)
+
+func ReceiveDamage(amount,type):
+	Health.TakeDamage(amount,type)
+	entity_sprite.play("hurt")
+
+
+
+func Death():
+	queue_free()
+
+
+func _on_entity_sprite_animation_finished():
+	print_debug(Health.CurrentHP)
+	if Health.CurrentHP > 0:
+		print_debug("SHould Play")
+		entity_sprite.play("Stand")
+
+func AttackAnim():
+	entity_sprite.play("hit")
+
+func MobAttack():
+	attack.MobAttack()
+
+
+func _on_chef_animation_finished() -> void:
+	pass # Replace with function body.
